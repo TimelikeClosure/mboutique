967d80e83a27bbfd8035d7dbc46ec560d16ca2ba
Tim Horist Jr <Tim Horist Jr>
2016-03-13 14:55:57 -0700

    Added separate error.php to display 404 errors

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <?php
                 if (!array_key_exists($pageData['page'], $pageData['links'])) {
                     http_response_code(404);
-                    echo "<h1>404 Not Found</h1>";
+                    include('pages/components/404notfound.php');
                 } else {
                     include($pageData['links'][$pageData['page']]['url']);
                 }
